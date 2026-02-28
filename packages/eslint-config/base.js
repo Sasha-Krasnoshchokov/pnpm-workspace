3eb47232dcdc41bc52913cfd65e5e20317bce38f
Oleksandr Krasnoshchokov <87876965+Sasha-Krasnoshchokov@users.noreply.github.com>
2026-03-01 00:28:41 +0200

    Add global ignores for ESLint configuration

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,11 +1,13 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import { globalIgnores } from 'eslint/config';
 import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   prettierConfig,
+  globalIgnores(['dist']),
   {
     rules: {
       "no-unused-vars": "off",
