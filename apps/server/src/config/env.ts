ed086054096038b8ead98248b82e7ae7dcfdfbc2
molot <molot.738@gmail.com>
2026-04-12 10:05:25 +0300

    chore(server): setup .envs, install fastify deps

--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -3,8 +3,10 @@ import 'dotenv/config';
 
 export const serverEnvSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
-  PORT: z.string().transform(Number).default(3210),
+  PORT: z.coerce.number().default(3210),
   API_PREFIX: z.string().default('/api/v1'),
+  DATABASE_URL: z.string().url(),
+  CORS_ORIGINS: z.string().transform((str) => str.split(',')),
 });
 
 export const env = serverEnvSchema.parse(process.env);
